62e1315adc61a386fe02dc3c56455be07f544b32
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2025-08-25 13:25:41 +0300

    Fixed length issues, added dynamic array function

---
+++ cdarr.hpp
@@ -0,0 +1,45 @@
+/*
+     NOTICE:
+          Library for dynamic arrays not fully done and tested, use it on your own risk
+*/
+
+
+#pragma once
+
+#include <iostream> // for output
+#include <cctype> // for checking type
+#include <algorithm> // for std::copy
+
+template <typename T>
+inline void clear_d(T *arr) {
+    for (int i = 0; arr[i] != '\0'; ++i) {
+        arr[i] = (char)0;
+    }
+}
+
+
+inline void push_back_d(char *arr, char what_push) {
+     // (sizeof(arr) / sizeof(*arr)) - can do same as S
+     int real_size = 0;
+     
+     for (int j = 0; (unsigned int)arr[j] != '\0'; ++j) {
+          if ((unsigned int)arr[j] != 0) {
+               real_size++;
+          } 
+     }
+     char new_arr[real_size + 1]{};
+     int counter = real_size;
+
+     for (int i = 0; i < (real_size + 1); ++i) {
+          new_arr[i] = arr[i];
+     }
+
+     while ((unsigned int)new_arr[counter - 1] == 0) {
+          --counter;
+     }
+
+     new_arr[real_size] = what_push;
+     new_arr[real_size + 1] = '\0';
+     clear_d(arr);
+     std::copy(new_arr, new_arr + (real_size + 1), arr); // NOTICE THIS OPERATION IS FOR ARRAY THAT ARE HAVE ENOUGH SPACE FOR EXTRA VALUES
+}
