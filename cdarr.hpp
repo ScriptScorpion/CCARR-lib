d069800b641f5058903f3413242c5acb397bc8ee
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2025-08-25 17:33:33 +0300

    Added new command for dynamic arrays

--- cdarr.hpp
+++ cdarr.hpp
@@ -40,3 +40,24 @@ inline void push_back_d(char *arr, char what_push) {
      clear_d(arr);
      std::copy(new_arr, new_arr + (real_size + 1), arr); // NOTICE THIS OPERATION IS FOR ARRAY THAT ARE HAVE ENOUGH SPACE FOR EXTRA VALUES
 }
+
+inline void push_front_d(char *arr, char what_push) {
+     // (sizeof(arr) / sizeof(*arr)) - can do same as S
+     int real_size = 0;
+     for (int j = 0; (unsigned int)arr[j] != '\0'; ++j) {
+          if ((unsigned int)arr[j] != 0) {
+               real_size++;
+          }
+     }
+     char new_arr[real_size + 1]{}; // 1 - cuz new null terminator(new terminator gets replaced with character, and we put null terminator at the end)
+     int count = real_size - 1; // for counting last values of array, -1 because arrays starts with 0
+     
+     for (int i = real_size; i > 0; --i) {
+          new_arr[i] = arr[count];
+          count--;
+     }
+     new_arr[0] = what_push;
+     new_arr[real_size + 1] = '\0';
+     clear_d(arr);
+     std::copy(new_arr, new_arr + (real_size + 1), arr); // NOTICE THIS OPERATION IS FOR ARRAY THAT ARE HAVE ENOUGH SPACE FOR EXTRA VALUES
+}
