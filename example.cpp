124b207a48f42a61189065f7111eb911e49ea05e
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2025-08-25 13:23:45 +0300

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -3,14 +3,13 @@
         size of array needed to be bigger by the amount of character you need to input,
 */
 #include "ccarr.hpp"
-
 int main() {
     const int size = 20;
     char arr[size] = {'a', 'b', 'c', 'd'};
     push_front(arr, std::string("w")); // passed checks!
     std::cout << arr << std::endl; // passed checks!
-    push_back(arr, std::string("w")); // not passed all checks
-    std::cout << arr << std::endl; // not passed all checks
+    push_back(arr, std::string("w")); // passed checks!
+    std::cout << arr << std::endl; // passed checks!
     push_front(arr, 'w'); // passed checks!
     std::cout << arr << std::endl; // passed checks!
     push_back(arr, 'w');  // passed checks!
